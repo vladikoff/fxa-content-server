2428fe364464413c29656b4cc7d106c0f20dcfdb
Vlad Filippov <vlad.filippov@gmail.com>
2015-10-15 17:16:43 -0400

    feat(csp): enable report only CSP in prodiction
    Fixes #1426

---
+++ server/lib/routes/post-csp.js
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Route to report CSP Violations to metrics
+ */
+module.exports = function () {
+  return {
+    method: 'post',
+    path: '/_/csp-violation',
+    process: function (req, res) {
+      res.json({result: 'ok'});
+      if (! req.body || ! req.body['csp-report'] || ! req.body['csp-report']['blocked-uri']) {
+        return;
+      }
+
+      var today = new Date();
+      today.setMinutes(0, 0, 0);
+
+      var entry = {
+        blocked: req.body['csp-report']['blocked-uri'],
+        op: 'server.csp',
+        time: today.toISOString()
+      };
+
+      process.stderr.write(JSON.stringify(entry) + '\n');
+    }
+  };
+};
