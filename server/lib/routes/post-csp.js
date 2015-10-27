ca95e0e021a9f039b8dc8aad2163bccb72a4efbc
vladikoff <vlad.filippov@gmail.com>
2015-10-27 19:04:08 -0400

    wip

--- server/lib/routes/post-csp.js
+++ server/lib/routes/post-csp.js
@@ -5,12 +5,28 @@
 /**
  * Route to report CSP Violations to metrics
  */
-module.exports = function () {
+module.exports = function (options) {
+  function isRateLimited() {
+    var min = 0;
+    var max = 10;
+    var RATE_LIMIT = 1;
+    var rand = min + Math.floor(Math.random() * (max - min + 1));
+
+    return rand >= RATE_LIMIT;
+  }
+
   return {
     method: 'post',
     path: '/_/csp-violation',
     process: function (req, res) {
       res.json({result: 'ok'});
+
+      // TODO: Not sure how many CSP errors we will get
+      // To avoid overflowing Heka logs rate limit the logging
+      if (isRateLimited()) {
+        return;
+      }
+
       if (! req.body || ! req.body['csp-report'] || ! req.body['csp-report']['blocked-uri']) {
         return;
       }
